7eb08c0971b1b4f699acafc1a06b100e05a3c206
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-05-21 10:31:12 +0900

    04-33_Controller のテスト（モックなし）：404 Not Found

--- src/test/java/com/example/blog/web/controller/article/ArticleRestControllerNoMockTest.java
+++ src/test/java/com/example/blog/web/controller/article/ArticleRestControllerNoMockTest.java
@@ -12,8 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -48,6 +47,18 @@ class ArticleRestControllerNoMockTest {
                 .andExpect(jsonPath("$.content").value("body_999"))
                 .andExpect(jsonPath("$.createdAt").value("2022-01-02T03:04:05"))
                 .andExpect(jsonPath("$.updatedAt").value("2023-01-02T03:04:05"));
+    }
+
+    @Test
+    @DisplayName("GET /articles/{id}: 指定されたIDの記事が存在しないとき、404 NotFound をレスポンスする")
+    public void getArticle_return404() throws Exception {
+        // ## Arrange ##
+
+        // ## Act ##
+        var actual = mockMvc.perform(get("/articles/{id}", -999));
+
+        // ## Assert ##
+        actual.andExpect(status().isNotFound());
     }
 
 }
