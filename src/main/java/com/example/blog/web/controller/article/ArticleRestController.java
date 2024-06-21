d22570c78680051c2403ebf202d38ffef34ee4d6
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-06-21 20:09:29 +0900

    05-25_これまでの変更で壊れたテストを修正する

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -1,14 +1,11 @@
 package com.example.blog.web.controller.article;
 
 import com.example.blog.service.article.ArticleService;
+import com.example.blog.web.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +17,6 @@ public class ArticleRestController {
     public ArticleDTO showArticle(@PathVariable("id") long id) {
         return articleService.findById(id)
                 .map(ArticleDTO::from)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(ResourceNotFoundException::new);
     }
 }
