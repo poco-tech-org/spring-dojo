9326cb461e03d9764d8b6aacc2901e5cc9422a78
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-08-03 11:12:38 +0900

    06-39_ユーザー登録：バリデーション_UserFormTestでNGのパターンを追加する

--- src/test/java/com/example/blog/web/controller/user/UserFormTest.java
+++ src/test/java/com/example/blog/web/controller/user/UserFormTest.java
@@ -24,4 +24,18 @@ class UserFormTest {
         assertThat(actual).isEmpty();
     }
 
+    @Test
+    @DisplayName("username のバリデーション：失敗")
+    void username_failure() {
+        // ## Arrange ##
+        var validatorFactory = Validation.buildDefaultValidatorFactory();
+        var validator = validatorFactory.getValidator();
+        var cut = new UserForm(null, "password00");
+
+        // ## Act ##
+        var actual = validator.validate(cut);
+
+        // ## Assert ##
+        assertThat(actual).isNotEmpty();
+    }
 }
