ca0410fd11c97c7ec72c682e27ef694dc64cab62
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-08-23 15:27:27 +0900

    07-07_POST /articles：201 Created のテストを書く

---
+++ src/test/java/com/example/blog/web/controller/article/ArticleRestControllerTest.java
@@ -0,0 +1,52 @@
+package com.example.blog.web.controller.article;
+
+import com.example.blog.service.user.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class ArticleRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void setup() {
+        // ## Arrange ##
+
+        // ## Act ##
+
+        // ## Assert ##
+        assertThat(mockMvc).isNotNull();
+        assertThat(userService).isNotNull();
+    }
+
+    @Test
+    @DisplayName("POST /articles: 新規記事の作成に成功する")
+    void createArticles_201Created() throws Exception {
+        // ## Arrange ##
+
+        // ## Act ##
+        var actual = mockMvc.perform(
+                post("/articles")
+                        .with(csrf())
+        );
+
+        // ## Assert ##
+        actual.andExpect(status().isCreated());
+    }
+}
