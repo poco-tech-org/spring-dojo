343cf08d1596336c5e43e7b0dae03f4eaef35b51
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-09-04 15:41:37 +0900

    07-39_POST /articles：DateTimeService を導入してテスタビリティを向上させる

--- src/test/java/com/example/blog/service/article/ArticleServiceTest.java
+++ src/test/java/com/example/blog/service/article/ArticleServiceTest.java
@@ -2,13 +2,17 @@ package com.example.blog.service.article;
 
 import com.example.blog.config.MybatisDefaultDatasourceTest;
 import com.example.blog.repository.user.UserRepository;
+import com.example.blog.service.DateTimeService;
 import com.example.blog.service.user.UserEntity;
+import com.example.blog.util.TestDateTimeUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @MybatisDefaultDatasourceTest
 @Import(ArticleService.class)
@@ -18,6 +22,8 @@ class ArticleServiceTest {
     private UserRepository userRepository;
     @Autowired
     private ArticleService cut;
+    @MockBean
+    private DateTimeService mockDateTimeService;
 
     @Test
     void setup() {
@@ -35,6 +41,9 @@ class ArticleServiceTest {
         expectedUser.setEnabled(true);
         userRepository.insert(expectedUser);
 
+        var expectedCurrentDateTime = TestDateTimeUtil.of(2020, 1, 2, 10, 20, 30);
+        when(mockDateTimeService.now()).thenReturn(expectedCurrentDateTime);
+
         var expectedTitle = "test_article_title";
         var expectedBody = "test_article_body";
 
@@ -49,7 +58,7 @@ class ArticleServiceTest {
         assertThat(actual.getAuthor().getUsername()).isEqualTo(expectedUser.getUsername());
         assertThat(actual.getAuthor().getPassword()).isNull();
         assertThat(actual.getAuthor().isEnabled()).isEqualTo(expectedUser.isEnabled());
-        assertThat(actual.getCreatedAt()).isNotNull();
-        assertThat(actual.getUpdatedAt()).isEqualTo(actual.getCreatedAt());
+        assertThat(actual.getCreatedAt()).isEqualTo(expectedCurrentDateTime);
+        assertThat(actual.getUpdatedAt()).isEqualTo(expectedCurrentDateTime);
     }
 }
