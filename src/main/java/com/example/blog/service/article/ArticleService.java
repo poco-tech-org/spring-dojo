4fa87748aaf756b1dd7a14fc74d97956b155673c
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-09-04 13:28:55 +0900

    07-33_POST /articles：ArticleService#create を実装する

--- src/main/java/com/example/blog/service/article/ArticleService.java
+++ src/main/java/com/example/blog/service/article/ArticleService.java
@@ -1,10 +1,12 @@
 package com.example.blog.service.article;
 
 import com.example.blog.repository.article.ArticleRepository;
+import com.example.blog.service.user.UserEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.Optional;
 
 @Service
@@ -15,5 +17,20 @@ public class ArticleService {
 
     public Optional<ArticleEntity> findById(long id) {
         return articleRepository.selectById(id);
+    }
+
+    @Transactional
+    public ArticleEntity create(long userId, String title, String body) {
+        var timestamp = OffsetDateTime.now();
+        var newEntity = new ArticleEntity(
+                null,
+                title,
+                body,
+                new UserEntity(userId, null, null, true),
+                timestamp,
+                timestamp
+        );
+        articleRepository.insert(newEntity);
+        return newEntity;
     }
 }
