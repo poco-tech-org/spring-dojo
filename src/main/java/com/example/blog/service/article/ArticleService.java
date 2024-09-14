5ae2ed8c6b333f7ff757971331f0f8443e2e40ce
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-09-14 12:05:14 +0900

    08-19_PUT /articles/{id}：403 Forbidden ①_編集権限がないリソースに対するリクエストの実装をする

--- src/main/java/com/example/blog/service/article/ArticleService.java
+++ src/main/java/com/example/blog/service/article/ArticleService.java
@@ -2,7 +2,9 @@ package com.example.blog.service.article;
 
 import com.example.blog.repository.article.ArticleRepository;
 import com.example.blog.service.DateTimeService;
+import com.example.blog.service.exception.UnauthorizedResourceAccessException;
 import com.example.blog.service.user.UserEntity;
+import com.example.blog.service.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -43,22 +45,24 @@ public class ArticleService {
     }
 
     @Transactional
-    public Optional<ArticleEntity> update(
-            long userId,
+    public ArticleEntity update(
+            long loggedInUserId,
             long articleId,
             String updatedTitle,
             String updatedBody
     ) {
-        // TODO mock impl
-        return findById(articleId)
-                .map(entity -> {
-                    entity.setTitle(updatedTitle);
-                    entity.setBody(updatedBody);
-                    entity.setUpdatedAt(dateTimeService.now());
+        var entity = findById(articleId)
+                .orElseThrow(ResourceNotFoundException::new);
 
-                    articleRepository.update(entity);
+        if (entity.getAuthor().getId() != loggedInUserId) {
+            throw new UnauthorizedResourceAccessException();
+        }
 
-                    return entity;
-                });
+        entity.setTitle(updatedTitle);
+        entity.setBody(updatedBody);
+        entity.setUpdatedAt(dateTimeService.now());
+        articleRepository.update(entity);
+
+        return entity;
     }
 }
