cc7a29a654248e65c18f67572682c73010c155e1
ita-jp <28795978+ita-jp@users.noreply.github.com>
2025-01-24 23:38:51 +0900

    10-15_S3Client の設定を追加する

--- src/main/java/com/example/blog/service/StorageService.java
+++ src/main/java/com/example/blog/service/StorageService.java
@@ -2,11 +2,15 @@ package com.example.blog.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
+import java.net.URI;
 import java.time.Duration;
 import java.util.Map;
 
@@ -19,30 +23,49 @@ public class StorageService {
             String contentType,
             Long contentLength
     ) {
-        return createPresignedUrl("test-bucket", "test-key", Map.of());
+        return createPresignedUrl("profile-images", fileName, Map.of());
     }
 
     // ref. https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/examples-s3-presign.html#put-presigned-object-part1
-    private String createPresignedUrl(String bucketName, String keyName, Map<String, String> metadata) {
-        try (var presigner = S3Presigner.create()) {
-            PutObjectRequest objectRequest = PutObjectRequest.builder()
+    private String createPresignedUrl(
+            String bucketName,
+            String keyName,
+            Map<String, String> metadata
+    ) {
+        var builder = S3Presigner.builder()
+                .serviceConfiguration(
+                        S3Configuration
+                                .builder()
+                                .pathStyleAccessEnabled(true)
+                                .build()
+                )
+                .endpointOverride(
+                        URI.create("http://localhost:4566")
+                )
+                .credentialsProvider(
+                        StaticCredentialsProvider
+                                .create(
+                                        AwsBasicCredentials
+                                                .create("test1", "test2")
+                                )
+                )
+                .region(
+                        Region.of("ap-northeast-1")
+                );
+
+        try (var presigner = builder.build()) {
+            var objectRequest = PutObjectRequest.builder()
                     .bucket(bucketName)
                     .key(keyName)
                     .metadata(metadata)
                     .build();
 
-            PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
-                    .signatureDuration(Duration.ofMinutes(10))  // The URL expires in 10 minutes.
+            var presignRequest = PutObjectPresignRequest.builder()
+                    .signatureDuration(Duration.ofMinutes(10))
                     .putObjectRequest(objectRequest)
                     .build();
 
-
-            PresignedPutObjectRequest presignedRequest = presigner.presignPutObject(presignRequest);
-            String myURL = presignedRequest.url().toString();
-            log.info("Presigned URL to upload a file to: [{}]", myURL);
-            log.info("HTTP method: [{}]", presignedRequest.httpRequest().method());
-
-            return presignedRequest.url().toExternalForm();
+            return presigner.presignPutObject(presignRequest).url().toString();
         }
     }
 }
