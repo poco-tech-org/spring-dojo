4cde8f56454cb06b540dfd761f7e2f6ed6ba284e
ita-jp <28795978+ita-jp@users.noreply.github.com>
2025-04-22 10:28:36 +0900

    10-22_S3Presigner を Bean 登録して DI するようにリファクタリングする

--- src/main/java/com/example/blog/service/StorageService.java
+++ src/main/java/com/example/blog/service/StorageService.java
@@ -1,53 +1,21 @@
 package com.example.blog.service;
 
 import com.example.blog.config.S3Properties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
-import java.net.URI;
 import java.time.Duration;
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class StorageService {
 
     private final S3Properties s3Properties;
     private final S3Presigner s3Presigner;
-
-    public StorageService(S3Properties s3Properties) {
-        this.s3Properties = s3Properties;
-        this.s3Presigner = S3Presigner.builder()
-                .serviceConfiguration(
-                        S3Configuration.builder()
-                                .pathStyleAccessEnabled(true)
-                                .build()
-                )
-                .endpointOverride(
-                        URI.create(
-                                s3Properties.endpoint()
-                        )
-                )
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(
-                                        s3Properties.accessKey(),
-                                        s3Properties.secretKey()
-                                )
-                        )
-                )
-                .region(
-                        Region.of(
-                                s3Properties.region()
-                        )
-                )
-                .build();
-    }
 
     public String createUploadURL(
             String fileName,
